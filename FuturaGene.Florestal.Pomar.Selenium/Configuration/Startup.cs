8f8f69362804eb316e584d1a40ff1fbd779aee63
Leonardo Barbetta <leo_barbetta@outlook.com>
2017-10-24 18:17:49 -0200

    Commit inicial

---
+++ FuturaGene.Florestal.Pomar.Selenium/Configuration/Startup.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace FuturaGene.Florestal.Pomar.Selenium.Configuration
+{
+    public class Startup
+    {
+        protected static IWebDriver Driver { get; set; }
+        protected WebDriverWait wait;
+
+        protected void SetUpAdmin()
+        {
+            if (Driver == null)
+                Driver = new ChromeDriver();
+
+            wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+
+            Driver.Manage().Window.Maximize();
+
+            Driver.Navigate().GoToUrl("http://localhost:63311/account/Sair");
+
+            wait.Until(ExpectedConditions.UrlContains("http://localhost:63311/"));
+
+            IWebElement txtUsuario = Driver.FindElement(By.Id("usuario"));
+            IWebElement txtSenha = Driver.FindElement(By.Id("senha"));
+            IWebElement btnEntrar = Driver.FindElement(By.XPath("//input[@value='Entrar']"));
+
+            txtUsuario.Clear();
+            txtUsuario.SendKeys("adminpomar");
+
+            txtSenha.Clear();
+            txtSenha.SendKeys("pomaradmin");
+
+            btnEntrar.Click();
+
+            wait.Until(ExpectedConditions.UrlContains("http://localhost:63311/Home"));
+        }
+
+        protected void TearDown()
+        {
+            try
+            {
+                Driver.Quit();
+                Driver = null;
+            }
+            catch (Exception)
+            { }
+        }
+    }
+}
---
+++ FuturaGene.Florestal.Pomar.Selenium/TestCases/Material/EspecieTest.cs
@@ -0,0 +1,45 @@
+﻿using FuturaGene.Florestal.Pomar.Selenium.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using Xunit;
+
+namespace FuturaGene.Florestal.Pomar.Selenium.TestCases.Material
+{
+    public class EspecieTest : Startup, IDisposable
+    {
+        [Fact]
+        public void InsereEspecieSucesso()
+        {
+            Driver.Navigate().GoToUrl("http://localhost:63311/Especies");
+            wait.Until(ExpectedConditions.UrlContains("http://localhost:63311/Especies"));
+
+            IWebElement btnNovaEspecie = Driver.FindElement(By.CssSelector("button[class='btn btn-success center-block btn-fg square']"));
+            btnNovaEspecie.Click();
+
+            IWebElement btnCreate = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("btnCreate")));
+            IWebElement txtNomeEspecie = Driver.FindElement(By.Id("Nome"));
+
+            txtNomeEspecie.Clear();
+            txtNomeEspecie.SendKeys("Especie Desconhecida - 11");
+
+            btnCreate.Click();
+
+            IWebElement retorno = wait.Until(ExpectedConditions.ElementExists(By.ClassName("iziToast-color-green")));
+
+            Assert.Equal("Sucesso", retorno.Text);
+
+        }
+
+
+        public EspecieTest()
+        {
+            SetUpAdmin();
+        }
+
+        public void Dispose()
+        {
+            TearDown();
+        }
+    }
+}
